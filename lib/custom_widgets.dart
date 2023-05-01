769efd133274599e0d5391a9c6b63477f055e5d5
Max Kørner Andersen <maxkandersen@gmail.com>
2023-05-01 15:59:57 +0200

    Main layout done

---
+++ lib/custom_widgets.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ReusableCard extends StatelessWidget {
+
+  late final Color color;
+
+  ReusableCard({required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
+  }
+}
---
+++ lib/input_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:bmi_calculator/custom_widgets.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xFF1D1E33);
+const bottomContainerColor = Color(0xFFEB1555);
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(
+                      color: activeCardColor,
+                    ),
+                  ),
+                  Expanded(
+                    child: ReusableCard(
+                      color: activeCardColor,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ReusableCard(
+                color: activeCardColor,
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(
+                      color: activeCardColor,
+                    ),
+                  ),
+                  Expanded(
+                    child: ReusableCard(
+                      color: activeCardColor,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 15.0),
+              color: bottomContainerColor,
+              height: bottomContainerHeight,
+              width: double.infinity,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
